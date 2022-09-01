5145e6e07b91db63007c904defb1f270cbbb6129
marceloCleaver <marcelo.morales2018@alu.uct.cl>
2022-09-01 00:04:53 -0400

    Cambios finales pre-presentación

--- Platinum-Bridge/src/app/app.module.ts
+++ Platinum-Bridge/src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PrincipalComponent } from './principal/principal.component';
 import { MainComponent } from './main/main.component';
 import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PrincipalComponent,
     MainComponent,
     ProfileComponent
   ],
