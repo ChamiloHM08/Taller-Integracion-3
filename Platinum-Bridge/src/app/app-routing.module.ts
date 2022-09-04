b610c636be19e6610030ea6f9d14ea81d42a1898
marceloCleaver <marcelo.morales2018@alu.uct.cl>
2022-09-03 23:52:22 -0400

    Enrutado inicial listo y funcionando.

--- Platinum-Bridge/src/app/app-routing.module.ts
+++ Platinum-Bridge/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { MainComponent } from './main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'profile', component: ProfileComponent },
+  { path: 'main', component: MainComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
