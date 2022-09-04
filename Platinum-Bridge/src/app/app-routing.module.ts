1665422cefc55a4eb56bfaa312d3ffc0d0776baf
marceloCleaver <marcelo.morales2018@alu.uct.cl>
2022-09-04 01:23:02 -0300

    Ultima versión

--- Platinum-Bridge/src/app/app-routing.module.ts
+++ Platinum-Bridge/src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProfileComponent } from './profile/profile.component';
 import { MainComponent } from './main/main.component';
-
+import { LoginComponent } from './login/login.component';
 const routes: Routes = [
   { path: 'profile', component: ProfileComponent },
-  { path: 'main', component: MainComponent }
+  { path: 'main', component: MainComponent },
+  { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
--- Platinum-Bridge/src/app/app.module.ts
+++ Platinum-Bridge/src/app/app.module.ts
@@ -4,16 +4,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    ProfileComponent
+    ProfileComponent,
+    LoginComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
