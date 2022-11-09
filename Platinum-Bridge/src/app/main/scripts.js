86f2e5afd3dac63d54a52088d5c35f086e97dd7b
Camilo Hernandez M <camilo.hernandez2020@alu.uct.cl>
2022-11-09 17:06:06 -0300

    Formulario Main - Api

--- Platinum-Bridge/src/app/main/scripts.js
+++ Platinum-Bridge/src/app/main/scripts.js
@@ -1,8 +1,34 @@
+
 function funcion1(){
     document.getElementById("vent").style.display="block";
-    console.log("aloo")
   }
 
   function funcion2(){
     document.getElementById("vent").style.display="none";
   }
+
+
+  /*
+  function funcion3(){
+    var inputs = document.getElementsByClassName('formulario__input');
+    for(var i=0; i<inputs.length; i++) {
+      if(this.value.length >= 1) {
+        this.nextElementSibling.classList.add('fijar');
+      }	else {
+        this.nextElementSibling.classList.remove('fijar');
+      }
+    }
+  }
+
+function Function4() {
+    var inputs = document.getElementsByClassName('formulario__input');
+    for(var i=0; i<inputs.length; i++){
+      if(inputs[i].value == ""){
+        alert("Complete todos los campos");
+        return false;
+      }
+    }
+    alert("Formulario enviado");
+    return true;
+  }
+*/
